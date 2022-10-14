6b0ba60b0d9aee091ee9b79d2b6fae4157ba7c80
jsakamoto <junichi.jp@gmail.com>
2022-10-14 22:48:52 +0900

    Improve PWA offline support.

--- InvokeDownloadAsync/InvokeDownloadAsyncExtension.cs
+++ InvokeDownloadAsync/InvokeDownloadAsyncExtension.cs
@@ -12,7 +12,14 @@ public static class InvokeDownloadAsyncExtension
             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
             .InformationalVersion ?? assembly.GetName().Version?.ToString() ?? "0.0.0";
 
-        await using var module = await jsRuntime.InvokeAsync<IJSObjectReference>("import", $"./_content/Toolbelt.Blazor.InvokeDownloadAsync/script.js?v={version}");
+        const string moduleScript = "export const f=()=>navigator.onLine;";
+        await using var inlineJsModule = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "data:text/javascript;charset=utf-8," + Uri.EscapeDataString(moduleScript));
+        var isOnLine = await inlineJsModule.InvokeAsync<bool>("f");
+
+        var scriptPath = "./_content/Toolbelt.Blazor.InvokeDownloadAsync/script.min.js";
+        if (isOnLine) scriptPath += $"?v={version}";
+
+        await using var module = await jsRuntime.InvokeAsync<IJSObjectReference>("import", scriptPath);
         await module.InvokeVoidAsync("invokeDownload", fileName, contentType, contents);
     }
 }
